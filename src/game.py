6eb7a980decb51f12766664d6a55bb9b8fa5ade8
Tristan Valentin <tristan.valentin2002@gmail.com>
2024-12-11 12:28:04 +0100

    [ADD] Play button OK

--- src/game.py
+++ src/game.py
@@ -1,1 +1,22 @@
 import sys
+import pygame
+
+def game_loop(screen):
+    width, height = screen.get_size()
+
+    game_background = pygame.image.load("texture/game/game_background.jpeg")
+    game_background = pygame.transform.scale(game_background, (1280, 720))
+
+    # Game loop
+    game_running = True
+    while game_running:
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        screen.blit(game_background, (0, 0))
+        pygame.display.update()
+
+
--- src/menu.py
+++ src/menu.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import pygame_menu as pm
+from src.game import game_loop
 
 global user_name
 
@@ -12,7 +13,6 @@ def main_menu(screen):
     menu_background = pygame.image.load("texture/menu/background.png")
     menu_background = pygame.transform.scale(menu_background, (1280, 720))
 
-    #Theme transparent
     # Create a custom theme with transparent background
     custom_theme = pm.Theme(
         background_color=(0, 0, 0, 0),  # Fully transparent
@@ -23,15 +23,20 @@ def main_menu(screen):
     )
 
     # Create the menu
-    menu = pm.Menu('', width, height, theme=custom_theme)
+    menu = pm.Menu('', width, height, theme=custom_theme, center_content=False)
+
+    def start_game():
+        menu_running = False
+        game_loop(screen)  # Démarre la boucle de jeu
 
     # Add buttons to the menu
-    menu.add.button('Play', pm.events.NONE, screen)
+    menu.add.button('Play', start_game)
     menu.add.button('Options', pm.events.NONE, screen)
     menu.add.button('Quit', pm.events.EXIT, screen)
 
     # Menu loop
     menu_running = True
+
     while menu_running:
         events = pygame.event.get()
         for event in events:
