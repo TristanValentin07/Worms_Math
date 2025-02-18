d7965f3a86f35406994a4e39676b0e4162e923b2
Evan <evan.rktcisse@gmail.com>
2025-02-18 02:03:14 +0100

    [FIX] fixed running window

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
+import pygame
 from src.menu import *
 
-#Create Window
-
+#Initialize Pygame
 pygame.init()
-width = 1920
-height = 1080
-screen = pygame.display.set_mode((1920, 1080), pygame.FULLSCREEN)
+
+#Configure the window
+screen_width = 1920
+screen_height = 1080
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Worms Math")
+
+#Call the main menu
 main_menu(screen)
-pygame.quit()
